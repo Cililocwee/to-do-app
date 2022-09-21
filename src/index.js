c0fd2d13eddb2f895767a1615c657274ba2aee94
Cililocwee <corrie.stroup@gmail.com>
2022-09-21 18:33:35 +0700

    added functionality to the datetime input, added a priority value that defaults to low and adds a colored border-top based on the priority, imported datetime-fns for pretty dates

--- src/index.js
+++ src/index.js
@@ -1,43 +1,22 @@
 import './styles/style.css'
 import dropdown from './functions/dropdown';
 import memorycard from './functions/memorycard';
+import { compareAsc, format } from 'date-fns';
 
 // selecting the viewbox
 const viewBox = document.querySelector('.view-box')
 
-// whole date-card (the whole card)
-const dateCardDiv = document.createElement('div');
-dateCardDiv.classList.add('date-card');
-
-// current date display
-const dateDisplay = document.createElement('div');
-dateDisplay.classList.add('date-display');
-
 // date and formatting (short form)
 var utc = new Date().toJSON().slice(0, 10).replace(/-/g, '-');
-dateDisplay.innerText = utc;
+let currentdate = format(new Date(), 'MM/dd/yyyy');
+let currettime = format(new Date(), 'h:mmaaaa');
+console.log(utc);
+console.log(currentdate);
 
-// todo container
-const toDoContainer = document.createElement('div');
-toDoContainer.classList.add('to-do-container');
-toDoContainer.id = 'to-do-container';
+// selecting the todocontainer
+const toDoContainer = document.querySelector('.to-do-container');
 
-// example posts
-const toDoItem1 = document.createElement('div');
-toDoItem1.classList.add('to-do-item');
-toDoItem1.innerHTML = "<p>This is the text in the first div.</p>";
-const toDoItem2 = document.createElement('div');
-toDoItem2.classList.add('to-do-item');
-toDoItem2.innerHTML = "<p>And this is more text, but in a different div.</p>"
-const toDoItem3 = document.createElement('div');
-toDoItem3.classList.add('to-do-item');
-toDoItem3.innerHTML = "<p>I, too, am a div, my dear fellows.</p>"
-
-// creates the input field for the todolets
-const todoinput = document.createElement('input');
-todoinput.type = 'text';
-todoinput.placeholder = 'Enter new note here';
-todoinput.id = 'to-do-input';
+const todoinput = document.querySelector('#to-do-input');
 todoinput.addEventListener('keypress', (event) => {
     if (event.key === 'Enter') {
         makeToDolet();
@@ -45,65 +24,60 @@ todoinput.addEventListener('keypress', (event) => {
     }
 });
 
-// creates the button that adds a new todolet
-const todoletbutton = document.createElement('button');
-todoletbutton.id = 'to-do-let-button';
-todoletbutton.classList.add('button-87')
-todoletbutton.type = 'button';
-todoletbutton.value = "add";
+// select the todolet button and add functionality
+const todoletbutton = document.querySelector('.to-do-let-button');
 todoletbutton.onclick = makeToDolet;
 todoletbutton.addEventListener('click', () => {
     todoinput.value = '';
 })
 
-// creates the timedate input element
-const datetimeinput = document.createElement('input');
-datetimeinput.id = 'datetimeinput';
-datetimeinput.name = 'datetimeinput';
-datetimeinput.type = 'datetime-local';
-datetimeinput.required = true;
-
 // (FUNC) creates a new todolet with value from input box and posts it
 function makeToDolet() {
+    // length limiter
     if (todoinput.value.length < 1) { return };
+    
     // create the todolet form
     const newToDoLet = document.createElement('div');
     newToDoLet.innerHTML = `<p>${todoinput.value}</p>`;
     newToDoLet.classList.add('to-do-item');
-    //newToDoLet.onclick = memorycard.removeItem;
+
     // create the new datelet (current date/time)
     const newDateLet = document.createElement('div');
     newDateLet.classList.add('datelet');
-    newDateLet.innerText = utc;
+    newDateLet.innerText = `Posted: ${currentdate} at ${currettime}`;
+
     // create the new duelet (due date/time)
     const newDueLet = document.createElement('div');
     newDueLet.classList.add('duelet');
     newDueLet.innerText = `Due: ${document.querySelector('#datetimeinput').value}`;
 
-    // remove button
+    // create the remove button
     const removebutton = document.createElement('h4');
     removebutton.innerText = 'x'
     removebutton.classList.add('remove-button');
     removebutton.onclick = memorycard.removeItem;
 
+    // get value from radio buttons
+    function displayRadioValue() {
+        var radioButtonGroup = document.getElementsByName('optradio');
+        var checkedRadio = Array.from(radioButtonGroup).find((radio) => radio.checked);
+        return checkedRadio.value;
+    }
+
     // **NOTES: This right here is ACTIVELY attaching 
     // as a consequence of calling this function
     toDoContainer.appendChild(newToDoLet);
     newToDoLet.append(newDateLet, newDueLet, removebutton);
-}
 
-// constructing the page below the header
-viewBox.appendChild(dateCardDiv);
-dateCardDiv.append(dateDisplay,
-    todoinput,
-    datetimeinput,
-    todoletbutton,
-    toDoContainer);
-//toDoContainer.append(toDoItem1,
-//    toDoItem2,
-//    toDoItem3);
+    // assign priority based on selection
+    newToDoLet.classList.add(`${displayRadioValue()}`)
+    
+}
 
 // adds functionality to the dropdown menu
 const dropbtn = document.querySelector('.dropbtn');
 dropbtn.onclick = dropdown.myFunction;
 
+// sets default datetime for the input (not currently working)
+const datetimeinput = document.querySelector('#datetimeinput');
+datetimeinput.value= format(new Date(), ('yyyy-MM-ddThh:mm'));
